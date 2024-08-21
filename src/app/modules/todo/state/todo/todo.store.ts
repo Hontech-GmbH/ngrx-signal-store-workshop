80cb3e8f235e1fc42f6bad590f07dad21a8c7a03
Tim Honermann <gast.tim.honermann@suva.ch>
2024-08-21 08:20:21 +0200

    optimize imports

--- src/app/modules/todo/state/todo/todo.store.ts
+++ src/app/modules/todo/state/todo/todo.store.ts
@@ -1,19 +1,13 @@
-import { computed, inject } from '@angular/core';
-import { tapResponse } from '@ngrx/operators';
-import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from "@ngrx/signals";
-import { rxMethod } from '@ngrx/signals/rxjs-interop';
-import { pipe, switchMap } from 'rxjs';
-import { Todo } from '../../models/todo.models';
-import { TodoService } from '../../services/todo.service';
+import { signalStore, withHooks, withState } from '@ngrx/signals';
 import { initialState } from './todo.state';
-import { withTodoMethods } from "./todo.methods";
-import { withTodoSelectors } from "./todo.selectors";
+import { withTodoMethods } from './todo.methods';
+import { withTodoSelectors } from './todo.selectors';
 
 export const TodoStore = signalStore(
-    withState(initialState),
-    withTodoMethods(),
-    withTodoSelectors(),
-    withHooks({
-      onInit: (store) => store.loadAll()
-    })
-)
+  withState(initialState),
+  withTodoMethods(),
+  withTodoSelectors(),
+  withHooks({
+    onInit: (store) => store.loadAll(),
+  }),
+);
