a4c37c01e42b011a7c60e2effb63d627fe77310f
Tim Honermann <gast.tim.honermann@suva.ch>
2024-08-21 08:16:25 +0200

    added hook

--- src/app/modules/todo/state/todo/todo.store.ts
+++ src/app/modules/todo/state/todo/todo.store.ts
@@ -1,6 +1,6 @@
 import { computed, inject } from '@angular/core';
 import { tapResponse } from '@ngrx/operators';
-import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
+import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from "@ngrx/signals";
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { pipe, switchMap } from 'rxjs';
 import { Todo } from '../../models/todo.models';
@@ -78,5 +78,8 @@ export const TodoStore = signalStore(
                 ))
             )
         )
-    }))
+    })),
+    withHooks({
+      onInit: (store) => store.loadAll()
+    })
 )
